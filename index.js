bf0ac007d6cb179ddea036af204bc9da53d4ef31
Muhammad Hamza Hijazi <ihijazi313@gmail.com>
2023-07-26 03:04:52 +0500

    feat: add users api

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const PORT = 3000;
 app.get("/",(req,res)=>{
     return res.status(200).json({message:"API is running"})
 })
+app.get("/api/users",(req,res)=>{
+    return res.status(200).json({
+        message:"no user found",
+        data:[]
+    })
+})
 app.listen(PORT, ()=>console.info(`App listening on PORT ${PORT}`))
